7b5753aa65523a353de1f1eabacc5492ffb966cb
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-21 02:26:25 +0200

    add null user check

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -2,6 +2,10 @@ import { createRouter } from './context';
 
 export const userRouter = createRouter().query('me', {
   async resolve({ ctx }) {
+    if (!ctx.user?.userId) {
+      return null;
+    }
+
     return await ctx.prisma.user.findFirst({
       where: {
         id: ctx.user.userId,
