03d1f4475c8b94e621444158a493892fd367df19
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-21 22:58:10 +0200

    try pwa on prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
-import { env } from "./src/env/server.mjs";
-
+import { env } from './src/env/server.mjs';
+import withPWA from 'next-pwa';
+console.log(env);
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -12,7 +13,18 @@ function defineNextConfig(config) {
   return config;
 }
 
+const wpaConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+});
+
+delete wpaConfig.pwa;
+
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  ...wpaConfig,
 });
