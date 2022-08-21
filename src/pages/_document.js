4e0b1af5514efe885a1f9a0839da8e149e6fc16e
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-22 00:34:13 +0200

    move manifests

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,12 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { env } from '../env/server.mjs';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel='manifest' href='/manifest.json' />
+          <link rel='manifest' href={env.NEXT_PUBLIC_PATH + '/manifest.json'} />
           <link rel='apple-touch-icon' href='/icon.png'></link>
           <meta name='theme-color' content='#fff' />
         </Head>
