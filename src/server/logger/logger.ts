3d47cff53407d619569e1196a564d45ca083d76f
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-21 15:09:37 +0200

    change to vercel env

--- src/server/logger/logger.ts
+++ src/server/logger/logger.ts
@@ -19,7 +19,7 @@ const pinoOptions: LoggerOptions = {
   },
   level: 'debug',
   base: {
-    env: process.env.NODE_ENV,
+    env: process.env.VERCEL_ENV,
     revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
   },
 };
