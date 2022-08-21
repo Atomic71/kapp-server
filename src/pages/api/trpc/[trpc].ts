8a5e76a2102a8d714a2a063dedc1a1b0b4992f93
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-21 18:40:23 +0200

    add onError stream to root trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,29 @@
+import { APP_STREAM_LOGGER } from './../../../server/logger/logger';
 // src/pages/api/trpc/[trpc].ts
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { appRouter } from "../../../server/router";
-import { createContext } from "../../../server/router/context";
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { appRouter } from '../../../server/router';
+import { createContext } from '../../../server/router/context';
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
+  onError: (ctx) => {
+    APP_STREAM_LOGGER.error(
+      {
+        NAME: ctx.error.name,
+        ORIGIN: 'ROUTER ROOT',
+        ERROR: {
+          msg: ctx.error.message,
+          path: ctx.path,
+        },
+        REQUEST: {
+          input: ctx.input,
+          headers: ctx.req.headers,
+        },
+      },
+      '123'
+    );
+    return {};
+  },
 });
