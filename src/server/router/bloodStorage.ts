db6a906396d1d9ff0468094c71304b1467ba9d72
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-22 12:14:00 +0200

    add log

--- src/server/router/bloodStorage.ts
+++ src/server/router/bloodStorage.ts
@@ -3,6 +3,7 @@ import { createRouter } from './context';
 
 export const bloodStorageRouter = createRouter().query('getInfo', {
   async resolve({ ctx }) {
+    console.log('fetching...');
     return await fetchBloodStorageState();
   },
 });
