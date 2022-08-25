75b8949bf1fc0bee7ca23e8cdb2ff30610326283
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-25 08:22:41 +0200

    add profile query

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from 'next';
+import { trpc } from '../utils/trpc';
 
 const Profile: NextPage = () => {
-  return <div>user</div>;
+  const userQuery = trpc.useQuery(['user.me']);
+  return <div>{userQuery.data?.id ?? 'loading...'}</div>;
 };
 
 export default Profile;
