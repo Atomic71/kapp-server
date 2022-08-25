2403e840c50b7c86932b40ef6c348178fb04826c
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-25 08:47:06 +0200

    improve layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: PropsWithChildren) {
       </Head>
       <div className='min-h-screen flex flex-col'>
         <Navbar />
-        <main className='flex-grow container'>{children}</main>
+        <main className='flex-grow container mx-auto'>{children}</main>
         <Footer />
       </div>
     </>
