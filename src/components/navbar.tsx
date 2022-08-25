2b849fc3e301f1184cbbaa5196b752bee83d7576
Andrej Tomic <tomicandrej71@gmail.com>
2022-08-25 08:22:21 +0200

    add link around navbar brand

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,19 +8,21 @@ const Navbar: React.FC = () => {
   const user = trpc.useQuery(['user.me']);
 
   return (
-    <nav className='flex justify-between'>
-      <div>
-        <Image
-          height={68}
-          width={150}
-          src={env.NEXT_PUBLIC_PATH + '/navbar-logo.svg'}
-          alt='kapp header logo'
-        />
+    <nav className='flex justify-between px-2 py-3'>
+      <div className='cursor-pointer'>
+        <Link href='/'>
+          <Image
+            height={68}
+            width={150}
+            src={env.NEXT_PUBLIC_PATH + '/navbar-logo.svg'}
+            alt='kapp header logo'
+          />
+        </Link>
       </div>
       {user.data && (
         <div className='flex'>
           <Link href={'/profile'}>
-            <button className='p-2 bg-green-800 text-white'>Profil</button>
+            <button>Profil</button>
           </Link>
           <LogoutCta onLogout={() => user.refetch()} />
         </div>
